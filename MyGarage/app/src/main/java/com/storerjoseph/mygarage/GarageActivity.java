fd31c042d824265434e208e2554e6e113dee5d5d
Joseph <gmremcorat@gmail.com>
2019-07-28 02:18:31 -0400

    Vehicles now appear on the custom vehicleAdapter view on the main 'Garage view'

--- MyGarage/app/src/main/java/com/storerjoseph/mygarage/GarageActivity.java
+++ MyGarage/app/src/main/java/com/storerjoseph/mygarage/GarageActivity.java
@@ -5,10 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.storerjoseph.mygarage.Fragments.AddFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +28,11 @@ public class GarageActivity extends AppCompatActivity {
     public static final String TAG = "GarageActivty";
     private NetworkClass networkClass;
     private FloatingActionButton fab;
+    private FirebaseDatabase database;
+    private DatabaseReference dataRef;
+    private ArrayList<Vehicle> vehicles;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,8 @@ public class GarageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         Intent launchedIntent = getIntent();
+        vehicles = new ArrayList<>();
+        listView = findViewById(R.id.listView);
 
         if (launchedIntent.hasExtra(MainActivity.Garage_EXTRA)){
             account = (GoogleSignInAccount) launchedIntent.getParcelableExtra(MainActivity.Garage_EXTRA);
@@ -36,6 +53,9 @@ public class GarageActivity extends AppCompatActivity {
             // since we have internet let's only set our onclicklistener for our floating action button if we do have internet
             fab.setOnClickListener(fabListener);
             // Load vehicles here
+            database = FirebaseDatabase.getInstance();
+            dataRef = database.getReference(account.getId()).child("vehicles");
+            dataRef.addValueEventListener(vehListener);
 
         }else{
             AlertDialog.Builder alert = new AlertDialog.Builder(this)
@@ -62,6 +82,49 @@ public class GarageActivity extends AppCompatActivity {
             Log.i(TAG, "onClick: Fab clicked + " + account.getEmail());
             getSupportFragmentManager().beginTransaction().add(R.id.fragView,AddFragment.newInstance(account)).addToBackStack("backstack").commit();
             fab.hide();
+
+        }
+    };
+
+    // event listener
+    ValueEventListener vehListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            vehicles.removeAll(vehicles);
+            Log.i(TAG, "onDataChange: On Data changed!");
+            // Get Post object and use the values to update the UI
+            Log.i(TAG, "onDataChange: Data changed " + dataSnapshot.getChildrenCount() );
+
+            for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                Vehicle vehicle = snapshot.getValue(Vehicle.class);
+                vehicles.add(vehicle);
+                reloadData();
+            }
+            // ...
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            // Getting Post failed, log a message
+            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            // ...
+        }
+    };
+
+    private void reloadData(){
+        VehicleAdapter vehicleAdapter = new VehicleAdapter(vehicles,this);
+        listView.setAdapter(vehicleAdapter);
+        listView.setOnItemClickListener(listclickListener);
+    }
+
+
+
+
+    ListView.OnItemClickListener listclickListener = new ListView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Vehicle selected = vehicles.get(position);
 
         }
     };
